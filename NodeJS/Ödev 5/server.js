c9bc916cdaa1473802d201634de0ad74ee74b7bf
tayfunobuz <130822357+tayfunobuz@users.noreply.github.com>
2023-11-24 16:51:59 +0300

    Create server.js

---
+++ NodeJS/Ödev 5/server.js
@@ -0,0 +1,33 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+
+    if (url === '/') {
+        res.writeHead(200, {"content-type": "text/html"})
+        res.write("<h2>Index sayfasına hoşgeldiniz.</h2>")
+    }
+
+    else if (url === '/hakkimda') {
+        res.writeHead(200, {"content-type": "text/html"})
+        res.write("<h2>Hakkımda sayfasına hoşgeldiniz.</h2>")
+    }
+
+    else if (url === '/iletisim') {
+        res.writeHead(200, {"content-type": "text/html"})
+        res.write("<h2>İletişim sayfasına hoşgeldiniz</h2>")
+    }
+
+    else {
+        res.writeHead(404, {"content-type": "text/html"})
+        res.write("<h2>404 Sayfa bulunamadı.</h2>")
+    }
+
+    res.end()
+})
+
+const port = 5000
+
+server.listen(port, () => {
+    console.log(`Sunucu port ${port} de başlatıldı.`)
+})
